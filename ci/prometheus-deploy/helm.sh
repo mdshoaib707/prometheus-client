ac5bf4ddfe45d680c3c3e76ac696a547c63289a1
mdshoaib707 <mdshoaib707@gmail.com>
2020-08-04 01:03:41 +0530

    Update helm.sh

--- ci/prometheus-deploy/helm.sh
+++ ci/prometheus-deploy/helm.sh
@@ -4,4 +4,4 @@ kubectl create ns app --dry-run=true -o yaml | kubectl apply -f -
 
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo update
-helm install prometheus-operator --namespace monitoring stable/prometheus-operator --debug
+helm upgrade --install prometheus-operator --values values.yml --namespace monitoring stable/prometheus-operator
